4487b6e23fdc8b9481982b2bcc22cb7a75d701c0
IvanYKM <ivan.cardel@ykm.com.mx>
2023-11-14 09:30:19 -0600

    Se realizan relaciones de las tablas

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+
+    // Relación polimófica
+
+    public function imageable(){
+        return $this->morphTo();
+    }
+
+
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\FuncCall;
 
 class Post extends Model
 {
     use HasFactory;
+
+    // Relación una a muchos inversa
+    public function user(){
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relación muchos a muchos
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // Relación uno a uno polimórfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
